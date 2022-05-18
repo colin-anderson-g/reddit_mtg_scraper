8f4bbf600a190ba3eaa69fcb77fe3278dc4f98d4
colin-anderson-g <colin.anderson.g@gmail.com>
2022-05-18 13:13:56 -0400

    file name fix

--- main.py
+++ main.py
@@ -2,17 +2,17 @@ import praw
 import psycopg2
 import re
 import setup
-import submission
-import comment
-import card
+import submissions
+import comments
+import cards
 
 #Loop over the first 10 "hot" submissions in the macigtcg subreddit
 for submission in setup.reddit.subreddit('magictcg').hot(limit=10):
-    submission.save(submission)
+    submissions.save(submission)
 
     for comment in submission.comments:
         if setup.repattern.search(comment.body):
-            comment.save(comment, submission.id)
+            comments.save(comment, submission.id)
 
             for card in setup.repattern.finditer(comment.body):
-                card.save(comment, setup.repattern)
+                cards.save(comment, setup.repattern)
